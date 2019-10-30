0e1c9a8c71b3ee97b1acc7d2cbd8e1c7747db4fe
Kamil Pałka <palkakamil1988@gmail.com>
2019-10-30 17:09:47 +0100

    initial commit

---
+++ src/layouts/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">SPA</div>;
+  }
+}
+
+export default App;
