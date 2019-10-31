cbe70c58f8628feb56b8df9eeca8e0b00ddc6c90
Kamil Pałka <palkakamil1988@gmail.com>
2019-10-31 19:48:27 +0100

    add start page articles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,36 @@
 import React from "react";
 
+import Article from "../components/Article";
+
+const articles = [
+  {
+    id: 1,
+    title: "Wiewiórki uciekają z lasów",
+    author: "Tomasz Dzik",
+    text:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nisi ipsum ad excepturi magni dolores quam quo, quae veritatis aspernatur facilis ipsam ut molestiae vero quidem quas nemo, a minus."
+  },
+  {
+    id: 2,
+    title: "Koty nie chodzą po ścianach",
+    author: "Anna Pingwin",
+    text:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nisi ipsum ad excepturi magni dolores quam quo, quae veritatis aspernatur facilis ipsam ut molestiae vero quidem quas nemo, a minus."
+  },
+  {
+    id: 3,
+    title: "Mój pies nie je mięsa",
+    author: "Roman Kloczkowski",
+    text:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nisi ipsum ad excepturi magni dolores quam quo, quae veritatis aspernatur facilis ipsam ut molestiae vero quidem quas nemo, a minus."
+  }
+];
+
 const HomePage = () => {
-  return <div>Strona startowa</div>;
+  const artList = articles.map(article => (
+    <Article key={article.id} {...article} />
+  ));
+  return <div className="home">{artList}</div>;
 };
 
 export default HomePage;
