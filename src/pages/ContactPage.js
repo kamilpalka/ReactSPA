22c8c2774d608b224d0215f4f65c5f05939d86e3
Kamil Pałka <palkakamil1988@gmail.com>
2019-10-31 20:14:13 +0100

    add form page and prompt

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,43 @@
 import React from "react";
+import "../styles/ContactPage.css";
+import { Prompt } from "react-router-dom";
 
-const ContactPage = () => {
-  return <div>Strona kontaktowa</div>;
-};
+class ContactPage extends React.Component {
+  state = {
+    value: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      value: ""
+    });
+  };
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Wpisz wiadomość"
+          ></textarea>
+          <button>Wyślij</button>
+        </form>
+        <Prompt
+          when={this.state.value}
+          message="Nie dokończyłeś formularza. Czy na pewno chcesz opuścić stronę?"
+        />
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
