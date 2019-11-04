2c72ed1144b04f9699e17383f493a1ec745d4bbe
Kamil Pałka <palkakamil1988@gmail.com>
2019-11-04 17:19:29 +0100

    Updates

---
+++ precache-manifest.07e641d2aa92191d0f1f07958cb839fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e178383d4f8797b36d39397456b91cc",
+    "url": "/ReactSPA/static/media/header4.1e178383.jpg"
+  },
+  {
+    "revision": "70a525873f4634f76bedde78d2ff9dab",
+    "url": "/ReactSPA/static/media/header3.70a52587.jpg"
+  },
+  {
+    "revision": "26a6830d8e79a8b255ccc7acd5879f5d",
+    "url": "/ReactSPA/static/media/header2.26a6830d.jpg"
+  },
+  {
+    "revision": "0decbbc9cb16c56ace9b82dce435f0af",
+    "url": "/ReactSPA/static/media/header1.0decbbc9.jpg"
+  },
+  {
+    "revision": "d614620e2272f698cb896014dc0c9f51",
+    "url": "/ReactSPA/static/media/error1.d614620e.jpg"
+  },
+  {
+    "revision": "8d9a1d965306f92b837b",
+    "url": "/ReactSPA/static/js/runtime~main.b946b7b7.js"
+  },
+  {
+    "revision": "54f649a7d928e1a51f45",
+    "url": "/ReactSPA/static/js/main.2f7da5c0.chunk.js"
+  },
+  {
+    "revision": "c9182c1567a0bbbc07fd",
+    "url": "/ReactSPA/static/js/2.557ed988.chunk.js"
+  },
+  {
+    "revision": "54f649a7d928e1a51f45",
+    "url": "/ReactSPA/static/css/main.af8c57ff.chunk.css"
+  },
+  {
+    "revision": "ac3018865b92b2902e210ee361febde9",
+    "url": "/ReactSPA/index.html"
+  }
+];
